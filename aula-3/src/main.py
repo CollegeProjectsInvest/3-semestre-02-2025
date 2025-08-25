ce1fe694b076111c8221ce5e67bf72be92c17bb7
Markimg22 <marquim.net.221200@hotmail.com>
2025-08-25 19:30:26 -0400

    aula-3

---
+++ aula-3/src/main.py
@@ -0,0 +1,33 @@
+# idade: int = 19
+# nome: str = "Marcos"
+
+# if idade == 18 and nome == "Marcos":
+#     print("Marcos tem 18 anos.")
+# else:
+#     print("Alguém não tem 18 anos")
+
+# nota: int = 4
+
+# if nota > 8:
+#     print("parabéns")
+# elif nota < 8 and nota > 5:
+#     print("Precisa melhorar")
+# else:
+#     print("Reprovado!")
+
+# for i in range(0, 5):
+#     print("Número: ", i)
+
+# for letra in "Python":
+#     print(letra)
+
+# for elementos in [1, 2, 3, 10, 9]:
+#     print(elementos)
+
+# while True:
+#     print("Infinito")
+
+contador: int = 0
+while contador <= 5:
+    print(contador)
+    contador += 1
